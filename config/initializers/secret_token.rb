2c2234ec3b27c064118c2e836ae91bc8c5cf4937
Maciej Rzasa <maciejrzasa@gmail.com>
2013-04-08 00:54:28 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrzTesting::Application.config.secret_token = '0c35cddb876ffeeb8dc100ed64996ac01d9822a475409b2eb8a9526a873c0d284aa221531b3a57569a37558ee08d7f507c6bb57b58631d57a397168cb8779b46'
