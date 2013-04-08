734c5b279ce5f2e1e8de4416da09908d84d001df
Maciej Rzasa <maciejrzasa@gmail.com>
2013-04-08 03:07:45 +0200

    removed capybara specs

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -3,22 +3,4 @@ require 'capybara/rspec'
 
 feature "Students CRUD" do
 
-  describe "index" do
-    it "has title" do
-      visit '/students'
-      expect(page).to have_content("Listing")
-    end
-  end
-
-  describe "new student" do
-    it "ads student" do
-      visit '/students'
-      expect(page).not_to have_content("Batory")
-      visit '/students/new'
-      fill_in "First name", with: "Stefan"
-      fill_in "Last name", with: "Batory"
-      find_button("Create Student").click
-      expect(page).to have_content("Batory")
-    end
-  end
 end
